fd63903027ce8ac4b0aec63a3e0e5d5c973cd616
ShifaRayeesa <98878113+ShifaRayeesa@users.noreply.github.com>
2022-03-04 21:17:38 +0530

    Add files via upload

---
+++ MethodOverriding.java
@@ -0,0 +1,37 @@
+public class MethodOverriding {
+    public static void main(String[] args)
+    {
+        Animal animal=new Animal();
+        Cat cat=new Cat();
+        Dog dog=new Dog();
+        animal.sound();
+        cat.sound();
+        dog.sound();
+    }
+}
+
+class Animal
+{
+    String category="Animal";
+    public void sound()
+    {
+        System.out.println(category+" makes sound");
+    }
+
+}
+class Cat extends Animal
+{
+    String animalName="Cat";
+    public void sound()
+    {
+        System .out.println("The "+animalName+" says Meow meow..");
+    }
+}
+class Dog extends Animal
+{
+    String animalName="Dog";
+    public void sound()
+    {
+        System .out.println("The "+animalName+" says Wow wow..");
+    }
+}
